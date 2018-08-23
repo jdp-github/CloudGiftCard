60596f941d328425de2c423ea528e6405e312073
jdp-github <jdp_mail@163.com>
2018-08-23 23:49:26 +0800

    订单接口

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,154 +4,155 @@ const app = getApp()
 var base64 = require('../../utils/base64.js');
 
 Page({
-    data: {
-        interval: 5000,
-        duration: 500,
-        imgSrc: [],
-        dataList: [],
-        cardList: []
-        // dataList: [
-        //     [{
-        //             id: 0,
-        //             imgUrl: '../../images/1.jpg',
-        //             text: "冰沁心甜夏天见"
-        //         },
-        //         {
-        //             id: 1,
-        //             imgUrl: '../../images/2.jpg',
-        //             text: "妈妈，我想对你说"
-        //         }
-        //     ],
-        //     [{
-        //             id: 2,
-        //             imgUrl: '../../images/3.jpg',
-        //             text: "哈哈哈"
-        //         },
-        //         {
-        //             id: 3,
-        //             imgUrl: '../../images/4.jpg',
-        //             text: "呵呵呵呵"
-        //         }
-        //     ],
-        //     [{
-        //             id: 4,
-        //             imgUrl: '../../images/5.jpg',
-        //             text: "4444444"
-        //         },
-        //         {
-        //             id: 5,
-        //             imgUrl: '../../images/1.jpg',
-        //             text: "5555555"
-        //         }
-        //     ],
-        //     [{
-        //             id: 6,
-        //             imgUrl: '../../images/5.jpg',
-        //             text: "6666"
-        //         },
-        //         {
-        //             id: 7,
-        //             imgUrl: '../../images/1.jpg',
-        //             text: "77777"
-        //         }
-        //     ],
-        // ],
-    },
+  data: {
+    interval: 5000,
+    duration: 500,
+    imgSrc: [],
+    dataList: [],
+    cardList: []
+    // dataList: [
+    //     [{
+    //             id: 0,
+    //             imgUrl: '../../images/1.jpg',
+    //             text: "冰沁心甜夏天见"
+    //         },
+    //         {
+    //             id: 1,
+    //             imgUrl: '../../images/2.jpg',
+    //             text: "妈妈，我想对你说"
+    //         }
+    //     ],
+    //     [{
+    //             id: 2,
+    //             imgUrl: '../../images/3.jpg',
+    //             text: "哈哈哈"
+    //         },
+    //         {
+    //             id: 3,
+    //             imgUrl: '../../images/4.jpg',
+    //             text: "呵呵呵呵"
+    //         }
+    //     ],
+    //     [{
+    //             id: 4,
+    //             imgUrl: '../../images/5.jpg',
+    //             text: "4444444"
+    //         },
+    //         {
+    //             id: 5,
+    //             imgUrl: '../../images/1.jpg',
+    //             text: "5555555"
+    //         }
+    //     ],
+    //     [{
+    //             id: 6,
+    //             imgUrl: '../../images/5.jpg',
+    //             text: "6666"
+    //         },
+    //         {
+    //             id: 7,
+    //             imgUrl: '../../images/1.jpg',
+    //             text: "77777"
+    //         }
+    //     ],
+    // ],
+  },
 
-    onLoad: function() {
-        var self = this;
-        // 轮播图
-        wx.request({
-            url: 'https://www.lytall.com/v1/carousels',
-            header: {
-                'content-type': 'application/json' // 默认值
-            },
-            success: function(res) {
-                var resObj = JSON.parse(res.data.data);
-                for (var i = 0, len = resObj.kvs.length; i < len; i++) {
-                    var itemObj = resObj.kvs[i];
-                    var item = JSON.parse(base64.decode(itemObj.value));
-                    // console.log(base64.decode(itemObj.value))
-                    for (var j = 0, lenj = item.cards.length; j < lenj; j++) {
-                        self.data.imgSrc[j] = item.cards[j].spec.logo;
-                    }
-                }
-                self.setData({
-                    imgSrc: self.data.imgSrc
-                })
-                // console.log(self.data.imgSrc)
-            }
-        })
-        // 卡片
-        wx.request({
-            url: 'https://www.lytall.com/v1/cards',
-            header: {
-                'content-type': 'application/json' // 默认值
-            },
-            success: function(res) {
-                var resObj = JSON.parse(res.data.data);
-                var rowList = [];
-                for (var i = 0, len = resObj.kvs.length, j = 0; i < len; i++) {
-                    var itemBase64 = resObj.kvs[i];
-                    var item = JSON.parse(base64.decode(itemBase64.value));
-                    // console.log(base64.decode(itemBase64.value))
-                    item.index = i;
-
-                    self.data.cardList[i] = item;
-
-                    if (i % 2 == 0) {
-                        rowList = [];
-                    }
-                    rowList[i % 2] = item;
-                    self.data.dataList[Math.floor(i / 2)] = rowList;
-                }
-                self.setData({
-                    dataList: self.data.dataList,
-                    cardList: self.data.cardList
-                })
-                // console.log(self.data.cardList)
-            }
-        })
-    },
-
-    changeProperty: function(e) {
-        var propertyName = e.currentTarget.dataset.propertyName
-        var newData = {}
-        newData[propertyName] = e.detail.value
-        this.setData(newData)
-    },
-
-    onShareAppMessage: function(res) {
-        if (res.from === 'button') {
-            // 来自页面内转发按钮
-            console.log(res.target)
+  onLoad: function() {
+    var self = this;
+    // 轮播图
+    wx.request({
+      url: 'https://www.lytall.com/v1/carousels',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function(res) {
+        var resObj = JSON.parse(res.data.data);
+        for (var i = 0, len = resObj.kvs.length; i < len; i++) {
+          var itemObj = resObj.kvs[i];
+          var item = JSON.parse(base64.decode(itemObj.value));
+          // console.log(base64.decode(itemObj.value))
+          for (var j = 0, lenj = item.cards.length; j < lenj; j++) {
+            self.data.imgSrc[j] = item.cards[j].spec.logo;
+          }
         }
-        return {
-            title: '礼道',
-            path: 'pages/index/index',
-            success: function(res) {
-                // 转发成功
-            },
-            fail: function(res) {
-                // 转发失败
-            }
-        }
-    },
-    navigate: function(e) {
-        // console.log(e)
-        var selectedIndex = e.currentTarget.dataset.id;
-        var selectedImgUrl;
-        for (var i = 0, len = this.data.cardList.length; i < len; i++) {
-            if (i == selectedIndex) {
-                this.data.cardList[i].isChecked = true;
-                selectedImgUrl = this.data.cardList[i].spec.logo;
-            } else {
-                this.data.cardList[i].isChecked = false;
-            }
-        }
-        wx.navigateTo({
-            url: '../gift/gift?text=' + e.currentTarget.dataset.text +
-                '&cardList=' + JSON.stringify(this.data.cardList) + '&selectedImgUrl=' + selectedImgUrl
+        self.setData({
+          imgSrc: self.data.imgSrc
         })
-    },
+        // console.log(self.data.imgSrc)
+      }
+    })
+    // 卡片
+    wx.request({
+      url: 'https://www.lytall.com/v1/cards',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function(res) {
+        var resObj = JSON.parse(res.data.data);
+        var rowList = [];
+        for (var i = 0, len = resObj.kvs.length, j = 0; i < len; i++) {
+          var itemBase64 = resObj.kvs[i];
+          var item = JSON.parse(base64.decode(itemBase64.value));
+          // console.log(base64.decode(itemBase64.value))
+          item.index = i;
+
+          self.data.cardList[i] = item;
+
+          if (i % 2 == 0) {
+            rowList = [];
+          }
+          rowList[i % 2] = item;
+          self.data.dataList[Math.floor(i / 2)] = rowList;
+        }
+        self.setData({
+          dataList: self.data.dataList,
+          cardList: self.data.cardList
+        })
+        // console.log(self.data.cardList)
+      }
+    })
+  },
+
+  changeProperty: function(e) {
+    var propertyName = e.currentTarget.dataset.propertyName
+    var newData = {}
+    newData[propertyName] = e.detail.value
+    this.setData(newData)
+  },
+
+  onShareAppMessage: function(res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '礼道',
+      path: 'pages/index/index',
+      success: function(res) {
+        // 转发成功
+      },
+      fail: function(res) {
+        // 转发失败
+      }
+    }
+  },
+  navigate: function(e) {
+    // console.log(e)
+    var selectedIndex = e.currentTarget.dataset.id;
+    var selectedCard = {};
+    for (var i = 0, len = this.data.cardList.length; i < len; i++) {
+      if (i == selectedIndex) {
+        this.data.cardList[i].isChecked = true;
+        selectedCard = this.data.cardList[i];
+      } else {
+        this.data.cardList[i].isChecked = false;
+      }
+    }
+
+    wx.navigateTo({
+      url: '../gift/gift?text=' + e.currentTarget.dataset.text +
+        '&cardList=' + JSON.stringify(this.data.cardList) + '&selectedCard=' + JSON.stringify(selectedCard)
+    })
+  },
 })
--- pages/login/login.js
+++ pages/login/login.js
@@ -1,117 +1,86 @@
 // pages/login/login.js
-var util = require('../../utils/util.js');
-var minMD5 = require('../../utils/minMD5.js');
-var RSA = require('../../utils/wxapp_rsa.js')
-var key = require('../../key/id_rsa.js');
-// var jsencrpt = require('../../utils/jsencrypt.min.js');
 
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
-        canIUse: wx.canIUse('button.open-type.getUserInfo')
-    },
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function(options) {
-        wx.getSetting({
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    wx.getSetting({
+      success: function(res) {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
             success: function(res) {
-                if (res.authSetting['scope.userInfo']) {
-                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-                    //   wx.getUserInfo({
-                    //     success: function(res) {
-                    //       wx.switchTab({
-                    //         url: '../index/index'
-                    //       })
-                    //     }
-                    //   })
-                }
+              wx.switchTab({
+                url: '../index/index'
+              })
             }
-        })
-    },
+          })
+        }
+      }
+    })
+  },
 
-    bindGetUserInfo: function(e) {
-        // wx.switchTab({
-        //   url: '../index/index'
-        // })
+  bindGetUserInfo: function(e) {
+    wx.switchTab({
+      url: '../index/index'
+    })
+  },
 
-        var encrypt_rsa = new RSA.RSAKey();
-        encrypt_rsa = RSA.KEYUTIL.getKey(key.public_key);
-        // debugger
-        // console.log('加密RSA:')
-        // console.log(encrypt_rsa)
-        var encStr = encrypt_rsa.encrypt("lytall.com/magicString/aaa.000000")
-        encStr = RSA.hex2b64(encStr);
-        // console.log("加密结果：" + encStr)
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
 
-        wx.request({
-            url: 'https://www.lytall.com/v1/orders',
-            header: {
-                'content-type': 'application/json', // 默认值
-                'Authorization': 'Bearer ' + encStr
-            },
-			// data:{
-			// 	uid:'ssss'
-			// },
-            success: function(res) {
-                console.log(res.data)
-            },
-            fail: function(res) {
-                console.log(res)
-            }
-        })
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function() {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
 
-    },
+  },
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
 
-    },
+  },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
 
-    },
+  },
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function() {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
 
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function() {
-
-    }
+  }
 })
